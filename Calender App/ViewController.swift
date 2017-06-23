5e848b994232b092e339381b6be6c71427cc099a
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-06-23 15:35:10 -0700

    Initial Commit

---
+++ Calender App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calender App
+//
+//  Created by Sebastian DiPirro on 6/23/17.
+//  Copyright © 2017 Sebastian DiPirro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
